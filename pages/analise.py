10b237cf733c23b864d6c1ccc29096f05e86abea
alexandresuehiro <alexandre.suehiro@gmail.com>
2024-10-01 19:57:50 -0300

    passos magicos

--- pages/analise.py
+++ pages/analise.py
@@ -5,6 +5,14 @@ import plotly.graph_objs as go
 from utils.constantes import TITULO_ANALISE_EXPLORATORIA, TITULO_PRINCIPAL
 from utils.layout import output_layout, format_number
 
+from tabs.analises.SobreIndicadoresAvaliacao_tab import SobreIndicadoresAvaliacao
+from tabs.analises.PerfilEstudantes_tab import PerfilEstudantes
+from tabs.analises.AnaliseIndicadoresPorPedra_tab import AnaliseIndicadoresPorPedra
+from tabs.analises.MapaCorrelacaoIndicadores_tab import MapaCorrelacaoIndicadores
+from tabs.analises.AnaliseTransicaoPedras_tab import AnaliseTransicaoPedras
+from tabs.analises.PontoVirada2022_tab import PontoVirada2022
+from tabs.analises.AnalisePontoVirada2022Sim_tab import AnalisePontoVirada2022Sim
+
 
 st.set_page_config(
     page_title=f"{TITULO_ANALISE_EXPLORATORIA} | {TITULO_PRINCIPAL}",
@@ -12,8 +20,38 @@ st.set_page_config(
 )
 output_layout()
 
-df = pd.read_csv("data/dataset.csv")
 
 with st.container():
     st.header(f":orange[{TITULO_ANALISE_EXPLORATORIA}]")
 
+    st.markdown(
+        """
+        Nesta sessão de análise, detalharemos um pouco sobre: 
+            1 - Sobre Indicadores de Avaliação: indicadores internos sobre o perfil de cada jovem
+            2 - Perfil dos Estudantes: análise de idade, tipo de escola, valor de INDE etc
+            3 - Análise dos Indicadores por Pedra: um escrutínio sobre a relevância de cada Indicador e o peso sobre o perfil das pedras
+            4 - 
+    """
+    )
+
+    tab0, tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(
+        tabs=[
+            "Sobre Indicadores de Avaliação",
+            "Perfil dos Estudantes",
+            "Análise dos Indicadores por Pedra",
+            "Mapa de Correlação de Indicadores",
+            "Análise de Transição de Pedras",
+            "Ponto de Virada 2022",
+            "Análise do Ponto de Virada 2022 = Sim"
+        ]
+    )
+
+    SobreIndicadoresAvaliacao(tab0)
+    PerfilEstudantes(tab1)
+    AnaliseIndicadoresPorPedra(tab2)
+    MapaCorrelacaoIndicadores(tab3)
+    AnaliseTransicaoPedras(tab4)
+    PontoVirada2022(tab5)
+    AnalisePontoVirada2022Sim(tab6)
+
+ 
--- tabs/intro/passos_magicos.py
+++ tabs/intro/passos_magicos.py
@@ -2,18 +2,17 @@ import streamlit as st
 from tabs.tab import TabInterface
 
 
-class IntroARIMA(TabInterface):
+class PassosMagicos(TabInterface):
     def __init__(self, tab):
         self.tab = tab
         self.render()
 
     def render(self):
         with self.tab:
-            st.subheader(":blue[Regressão Linear (Linear Regression)]", divider="blue")
+            st.subheader(":blue[Passos Mágicos]", divider="blue")
             st.markdown(
                 """
-                    Uma média móvel integrada autoregressiva, ou ARIMA, é um modelo de análise estatística que usa dados de séries temporais para compreender melhor o conjunto de dados ou para prever tendências futuras.
-                    Um modelo estatístico é autoregressivo se prever valores futuros com base em valores passados. Por exemplo, um modelo ARIMA pode procurar prever os preços futuros de uma ação com base no seu desempenho passado ou prever os lucros de uma empresa com base em períodos passados.
+                    Um pouco mais sobre a Metodologia da Passos Magicos.
                 """,
                 unsafe_allow_html=True,
             )
