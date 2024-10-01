fc0865b20f4fbb2ba38d084d4da57bef1129589b
alexandresuehiro <alexandre.suehiro@gmail.com>
2024-10-01 15:24:45 -0300

    change on constants page

--- utils/constantes.py
+++ utils/constantes.py
@@ -1,12 +1,12 @@
 from datetime import date
 
 
-TITULO_PRINCIPAL = 'Tech Challenge 4 | FIAP'
+TITULO_PRINCIPAL = 'Tech Challenge 5 | FIAP'
 TITULO_INTRODUCAO = 'Introdução'
-TITULO_HISTORIA = 'História sobre o preço'
+TITULO_HISTORIA = 'Sobre a Passos Mágicos'
 TITULO_ANALISE_EXPLORATORIA = 'Análise exploratória'
 TITULO_MODELO = 'Modelos de previsão'
 TITULO_REFLEXOES = 'Reflexões sobre as análises'
 TITULO_INSTRUCTIONS = 'Como usar o Streamlit'
 TITULO_REFERENCIAS = 'Referência Bibliográfica'
-DATA_INICIAL = date(2024, 5, 10)
+DATA_INICIAL = date(2024, 9, 25)
