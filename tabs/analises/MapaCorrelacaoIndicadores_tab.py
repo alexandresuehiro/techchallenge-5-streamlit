dca5531798c4cc09252927a7c311063b720652c3
alexandresuehiro <alexandre.suehiro@gmail.com>
2024-10-02 03:28:29 -0300

    commit

--- tabs/analises/MapaCorrelacaoIndicadores_tab.py
+++ tabs/analises/MapaCorrelacaoIndicadores_tab.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from tabs.tab import TabInterface
+import pandas as pd
 
 
 class MapaCorrelacaoIndicadores(TabInterface):
@@ -9,14 +10,32 @@ class MapaCorrelacaoIndicadores(TabInterface):
 
     def render(self):
         with self.tab:
-            st.subheader(":blue[Regressão Linear (Linear Regression)]", divider="blue")
+            st.header(":blue[Mapa de Correlação dos Indicadores]", divider="blue")
             st.markdown(
                 """
-                    Uma média móvel integrada autoregressiva, ou ARIMA, é um modelo de análise estatística que usa dados de séries temporais para compreender melhor o conjunto de dados ou para prever tendências futuras.
-                    Um modelo estatístico é autoregressivo se prever valores futuros com base em valores passados. Por exemplo, um modelo ARIMA pode procurar prever os preços futuros de uma ação com base no seu desempenho passado ou prever os lucros de uma empresa com base em períodos passados.
-                """,
-                unsafe_allow_html=True,
-            )
+                    Considerando os mapas, os indicadores psicológicos têm forte correlação entre si, além do indicador de engajamento. Pelo mapa, vemos que o IPP, IPV, IPS e IEG têm forte correlação, logo analisar o perfil dos estudantes pelas pedras e qual dos indicadores teriam maior peso poderia indicar um ponto a ser trabalhado.
+                """)
+            st.subheader('Mapa de Correlação dos Indicadores – 2020') 
+            st.image('assets/imgs/01_mapa_correlacao_2020.png')
+            st.markdown(
+                """
+                    Se olharmos o IPV como um fator crítico que determina a evolução do aprendizado e observamos as correlações maiores no seu entorno, podemos dizer que o IPP e o IPS têm peso considerável no ano de 2020.
+                """)
+
+
+            st.subheader('Mapa de Correlação dos Indicadores - 2021') 
+
+            st.image('assets/imgs/01_mapa_correlacao_2021.png')
+
+            st.markdown(
+                """
+                    O cenário em 2021 se repete ao de 2020, porém o IEG teve uma correlação maior (0.90  0.95), o que poderia implicar na compensação pela queda do IPP.
+                """) 
+
+            st.subheader('Mapa de Correlação dos Indicadores - 2022')                       
+  
+            st.image('assets/imgs/01_mapa_correlacao_2022.png')
+
 
             st.divider()
 
