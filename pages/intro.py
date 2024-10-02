200021a6e5c23014b8346f4487e6b6f84d0a26b8
alexandresuehiro <alexandre.suehiro@gmail.com>
2024-10-01 21:49:58 -0300

    commits

--- pages/intro.py
+++ pages/intro.py
@@ -2,12 +2,12 @@ import streamlit as st
 from tabs.intro.passos_magicos import PassosMagicos
 from tabs.intro.educacao_brasil import EducacaoBrasil
 #from tabs.intro.educacao_local import EducacaoLocal
-from tabs.intro.decision_tree_classification_method import expDecisionTreeClassification
-from tabs.intro.random_forest_classification_method import expRandomForestClassification
+#from tabs.intro.decision_tree_classification_method import expDecisionTreeClassification
+#from tabs.intro.random_forest_classification_method import expRandomForestClassification
 from tabs.intro.linear_regression_method import expLinearRegression
 from tabs.intro.logistic_regression_method import expLogisticRegression
-from tabs.intro.decision_tree_regression_method import expDecisionTreeRegression
-from tabs.intro.random_forest_regression_method import expRandomForestRegression
+from tabs.intro.decision_tree_method import expDecisionTree
+from tabs.intro.random_forest_method import expRandomForest
 
 from utils.constantes import TITULO_INTRODUCAO, TITULO_PRINCIPAL
 from utils.layout import output_layout
@@ -21,13 +21,27 @@ output_layout()
 with st.container():
     st.header(f":orange[{TITULO_INTRODUCAO}]")
 
-    st.markdown(
+    st.write(
         """
-        Nesta introdução, são descritos alguns tópicos importantes para entender a missão da Passos Mágicos e os modelos de Machine Learning usados: o Meta Prophet e o ARIMA.
+        Nesta introdução, são descritos alguns tópicos importantes para entender:
+         * Panorama da Educação Brasileira
+         * Sobre a Passos Mágicos:
+         <ol>
+         <li>Nossa História</li>
+         <li>O que fazemos</li>
+         <li>Missão e Valores</li>
+         </ol>
+         * os modelos de Machine Learning usados: 
+         <ol>
+         <li>Regressão Linear (Linear Regression)</li>
+         <li>Regressão Logística (Logistic Regression)</li>
+         <li>Árvores de Decisão (Decision Trees)</li>
+         <li>Florestas Aleatórias (Random Forest)</li>
+         </ol>
     """
     )
 
-    tab0, tab1, tab3, tab4, tab5, tab6, tab7, tab8 = st.tabs( #tab2, 
+    tab0, tab1, tab3, tab4, tab5, tab6 = st.tabs( #tab7, tab8  #tab2, 
     
         tabs=[
             "Passos Mágicos",
@@ -47,8 +61,8 @@ with st.container():
 #    EducacaoLocal(tab2)
     expLinearRegression(tab3)
     expLogisticRegression(tab4)
-    expDecisionTreeClassification(tab5)
-    expRandomForestClassification(tab6)
-    expDecisionTreeRegression(tab7)
-    expRandomForestRegression(tab8)
+    expDecisionTree(tab5)
+    expRandomForest(tab6)
+#    expDecisionTreeRegression(tab7)
+#    expRandomForestRegression(tab8)
  
